1885184d22edfc4d6bfef2d5e1efa46081b65b1b
Arthur Moore <Arthur.Moore.git@cd-net.net>
2015-03-27 13:30:48 -0400

    Debian build changes

--- debian-build.sh
+++ debian-build.sh
@@ -4,7 +4,7 @@
 #WARNING:  This will not work on debian wheezy.
 #          The network code was partially rewritten to use SFML2 which is not available on that distribution
 
-DEV_PACKAGES=( zlib1g-dev libpng-dev libgl1-mesa-dev libsdl1.2-dev libsfml-dev libgtkmm-2.4-dev libopenal-dev libwxgtk2.8-dev libgtkglextmm-x11-1.2-dev )
+DEV_PACKAGES=( zlib1g-dev libpng-dev libgl1-mesa-dev libsdl1.2-dev libsfml-dev libgtkmm-3.0-dev libopenal-dev libwxgtk2.8-dev )
 
 BUILD_PACKAGES=( build-essential cmake git ${DEV_PACKAGES[*]} zip )
 
